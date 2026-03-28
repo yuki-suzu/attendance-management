d5cdd0975ed4d10659e6837aec6923ca547a4c94
yuki suzuki <64686566+yuki-suzu@users.noreply.github.com>
2026-03-29 00:19:27 +0900

    初期構築 (#1)

---
+++ src/main/java/com/computer_rescuer/attendance_management/AttendanceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.computer_rescuer.attendance_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceManagementApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(AttendanceManagementApplication.class, args);
+  }
+
+}
---
+++ src/main/java/com/computer_rescuer/attendance_management/infrastructure/config/AppPropertyConfig.java
@@ -0,0 +1,10 @@
+package com.computer_rescuer.attendance_management.infrastructure.config;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan("com.computer_rescuer.attendance_management.infrastructure.property")
+public class AppPropertyConfig {
+
+}
