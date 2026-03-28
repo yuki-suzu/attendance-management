1eb20b880361590716d26ed99510b6d09d448b21
yuki-suzu <yukisuzuki8105@gmail.com>
2026-03-29 00:07:48 +0900

    初期構築

--- src/test/java/com/computer_rescuer/attendance_management/AttendanceManagementApplicationTests.java
+++ src/test/java/com/computer_rescuer/attendance_management/AttendanceManagementApplicationTests.java
@@ -2,12 +2,14 @@ package com.computer_rescuer.attendance_management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AttendanceManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+  }
 
 }
